4eda206df33fe85487074188a0b3c1a049733b73
waiyanhtun282 <waiyanhtun282@gmail.com>
2024-09-19 17:29:00 +0400

    carlet  Notesapp files css

---
+++ Notesapp/notes.js
@@ -0,0 +1,35 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector('.btn');
+let notes = document.querySelectorAll('.input-box');
+
+const showNotes = () =>{
+    notesContainer.innerHTML=localStorage.getItem('notes');
+}
+showNotes();
+const updateStorage = () =>{
+    localStorage.setItem('notes',notesContainer.innerHTML);
+}
+createBtn.addEventListener('click',() =>{
+    let inputBox =document.createElement('p');
+    let img = document.createElement('img');
+    inputBox.className='input-box';
+    inputBox.setAttribute("contenteditable","true");
+    img.src ='images/delete.png';
+    notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+notesContainer.addEventListener('click',(e) =>{
+if(e.target.tagName=== "IMG"){
+    e.target.parentElement.remove();
+    updateStorage();
+}else if(e.target.tagName === "P") {
+    notes =document.querySelectorAll('.input-box');
+    console.log(notes);
+    notes.forEach(nt => {
+        nt.onkeyup = () =>{
+            updateStorage();
+        }
+    })
+    
+}
+})
